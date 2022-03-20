d3d4b854d59accfc787a22f1807eab3b19830416
wbruns <wbruns07@gmail.com>
2022-03-20 13:24:39 -0500

    added functions for validating a note, creating a note with a uuid and a post route for creating notes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+// import functions
+const { createNewNote, validateNote } = require('../../lib/notes');
 // import db.json data
 const { notes } = require ('../../db/db.json');
 
@@ -7,6 +10,19 @@ router.get('/notes', (req, res) => {
     let results = notes;
     
     res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    // set id
+    req.body.id = uuidv4();
+    console.log(req.body.id);
+
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
 });
 
 module.exports = router;
