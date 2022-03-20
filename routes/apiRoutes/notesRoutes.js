99a88b2b2d268ce98ef48db495cd688a11e4c522
wbruns <wbruns07@gmail.com>
2022-03-19 20:16:26 -0500

    installed uuid package for creating unique id's

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
-// import functions
-// const { getNotes, saveNote, deleteNote } = require('../../public/assets/js/index');
 // import db.json data
 const { notes } = require ('../../db/db.json');
 
 // get all notes
 router.get('/notes', (req, res) => {
     let results = notes;
-    console.log(results);
+    
     res.json(results);
 });
 
